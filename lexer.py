203c6fb24f7f836cec6c7cf38d7f6441ceced6a3
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-23 12:36:57 -0300

    att

--- lexer.py
+++ lexer.py
@@ -32,8 +32,10 @@ def transform_content_into_readble_data(content:str)->dict:
     instructions = content.split('\n')
     instructions = list(map(lambda x: x.strip(),instructions))
     instructions = list(filter(lambda x: x != '', instructions))
-    instructions = list(map(lambda x : x.split(' '), instructions))
-    
+    instructions = list(map(lambda x : x.split(','), instructions))
+    instructions = list(map(
+        lambda x: list(map(lambda y: y.strip(), x)), 
+        instructions))
     instructions = list(map(lambda x: list(filter(lambda y: y != '', x)), instructions))
    
     for i in instructions:
--- teste.asm
+++ teste.asm
@@ -1,21 +1,21 @@
 
 
 
-mov  a  "Foda se"
-mov  state  print
+mov , a , "Foda se"
+mov , state , print
 syscall 
 
 
-mov a  "Digita Algo"
-mov state  print 
+mov ,a , "Digita Algo"
+mov, state , print 
 syscall 
 
-mov state read
+mov, state, read
 syscall
 
 
-mov a "Você digitou"
-mov state print 
+mov, a, "Você digitou"
+mov, state, print 
 syscall 
 
 
